1f4bcfd039fd0855bfa9dd2e4195902095fbb77d
mzoll <marcel.zoll.physics@gmail.com>
2019-02-03 17:57:44 +0100

    ready to consistently do getStateBuilders and getPrimeBuilders

--- tslearn/sklearnext/feature_dict.py
+++ tslearn/sklearnext/feature_dict.py
@@ -1,16 +1,12 @@
-'''
+"""
 Created on Jun 8, 2018
 
 @author: marcel.zoll
-'''
+"""
 
-from ..prime_building.utils import extractUniqueStateBuilders
-
+from ..state_building.utils import extractUniqueStateBuilders
 from sklearnext.feature_dict.facility import FeatureTransformFacility
 
 class FeatureTransformFacility(FeatureTransformFacility):
-    def getPrimeBuilders(self):
-        pb_list = [p for t in self.pipeline_dict.values() for p in t.getPrimeBuilders()]
-        return extractUniquePrimeBuilders(pb_list)
     def getStateBuilders(self):
         return extractUniqueStateBuilders(self.getStateBuilders())
