21464dd5520cd4900ae5ee3173c23dd63d8abfd7
mzoll <marcel.zoll.physics@gmail.com>
2019-02-12 22:14:42 +0100

    grand inkeys property to assembly pieces for quick lookup, what goes in and what comes out

--- tslearn/sklearnext/assembly.py
+++ tslearn/sklearnext/assembly.py
@@ -15,12 +15,19 @@ from sklearnext import assembly
 class ColumnsAll(assembly.ColumnsAll):
     def getStateBuilders(self):
         return []
+    @property
+    def inkeys(self):
+        return self.feature_names
 
 class ColumnsSelect(assembly.ColumnsSelect):
     def __init__(self, feature_names):
         assembly.ColumnsSelect.__init__(self, feature_names)
     def getStateBuilders(self):
         return []
+    @property
+    def inkeys(self):
+        return self.feature_names
+
 
 #=================
 # FeatureUnion
@@ -31,7 +38,14 @@ class FeatureUnion(assembly.FeatureUnion):
         for t in self.transformer_list:
             sb_set.update(t[1].getStateBuilders())
         return list(sb_set)
-    
+    @property
+    def inkeys(self):
+        inkey_set = set()
+        for t in self.transformer_list:
+            inkey_set.update(t[1].inkeys)
+        return inkey_set
+
+
 #=================
 # Pipeline
 #=================
@@ -45,11 +59,17 @@ class Pipeline(assembly.Pipeline):
         else:
             fnames = np.repeat( None, len(imps))
         return list(zip( fnames, imps ))
+    @property
+    def inkeys(self):
+        return self.steps[0][1].inkeys
 
 
 class TransformerPipe(assembly.TransformerPipe):
     def getStateBuilders(self):
         return self.steps[0][1].getStateBuilders()
+    @property
+    def inkeys(self):
+        return self.steps[0][1].inkeys
 
 
 class FeatureSelectPipeline(assembly.FeatureSelectPipeline):
@@ -63,17 +83,28 @@ class FeatureSelectPipeline(assembly.FeatureSelectPipeline):
         else:
             return self.getStateBuilders_null()
         return self.feature_union_apt.getStateBuilders()
-    
+    @property
+    def inkeys(self):
+        raise NotImplementedError()
+
+
 #==========================
 # Forks
 #==========================
 class CategoryFork(assembly.CategoryFork):
     def getStateBuilders(self):
         return self.pipeline.steps[0][1].getStateBuilders()
-        
+    @property
+    def inkeys(self):
+        raise NotImplementedError()
+
+
 class SplitterFork(assembly.SplitterFork):
     def getStateBuilders(self):
         sb0 = self.cat_trans.getStateBuilders()
         sb1 = self.pre_trans.getStateBuilders()
         sb2 = self.sub_pipe.getStateBuilders()
         return set(sb0) | set(sb1) | set(sb2)
+    @property
+    def inkeys(self):
+        raise NotImplementedError()
